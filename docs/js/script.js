4e1596195945aff3b80dcdee52e73fd72d6ed480
eduardormalves <139605693+eduardormalves@users.noreply.github.com>
2023-11-27 20:02:49 -0300

    Add files via upload

---
+++ docs/js/script.js
@@ -0,0 +1,12 @@
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) => {
+        if(entry.isIntersecting === true){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+elements.forEach( (elements) => myObserver.observe(elements))
